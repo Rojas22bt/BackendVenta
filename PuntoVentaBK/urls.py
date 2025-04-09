d9913b20ce71625db82b547393d1b5a47614e645
BoteritoROOT <d.sajor2003rp@gmail.com>
2025-04-09 17:43:41 -0400

    test

--- PuntoVentaBK/urls.py
+++ PuntoVentaBK/urls.py
@@ -21,4 +21,5 @@ from django.urls import include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('usuarios/',include('Usuarios.urls')),
+    path('api/', include('TestApp.urls')),
 ]
