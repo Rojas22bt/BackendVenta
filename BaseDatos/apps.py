73604d7e707d8fd7339c5b4ac0fea1b837a76b1d
BoteritoROOT <d.sajor2003rp@gmail.com>
2025-04-12 02:57:15 -0400

    hol

--- BaseDatos/apps.py
+++ BaseDatos/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class TestappConfig(AppConfig):
+class BasedatosConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'TestApp'
+    name = 'BaseDatos'
