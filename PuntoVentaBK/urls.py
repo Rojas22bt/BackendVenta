79d453c91e65f5de9865bc441217d2c3bf83a570
BoteritoROOT <d.sajor2003rp@gmail.com>
2025-04-12 03:04:34 -0400

    ss

--- PuntoVentaBK/urls.py
+++ PuntoVentaBK/urls.py
@@ -16,10 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from django.urls import include
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/usuarios2/', include('Usuarios2.urls')),
+
 ]
--- Usuarios2/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import UsuarioListCreateView, ClienteListCreateView,CustomTokenObtainPairView
-
-urlpatterns = [
-    path('usuarios/', UsuarioListCreateView.as_view(), name='usuario-list-create'),
-    path('clientes/', ClienteListCreateView.as_view(), name='cliente-list-create'),
-    path('login/', CustomTokenObtainPairView.as_view(), name='custom-login'), 
-]
