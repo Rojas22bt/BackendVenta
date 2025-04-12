412a60f5e0c66b9800f9317d2e677644e0454f98
BoteritoROOT <d.sajor2003rp@gmail.com>
2025-04-12 14:17:16 -0400

    document

--- userrs/serializers.py
+++ userrs/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from BaseDatos.models import Rol, Usuario, Documento
+from BaseDatos.models import Rol, Usuario, Documento, DetalleDocumento
 
 class RolSerializer(serializers.ModelSerializer):
     class Meta:
@@ -12,6 +12,10 @@ class DocumentoSerializer(serializers.ModelSerializer):
         fields = ['id','descripcion']
 
 class UsuarioRegistroSerializer(serializers.ModelSerializer):
+    documentos = serializers.ListField(
+        child=serializers.DictField(),
+        required=False
+    )
     password = serializers.CharField(write_only=True)
 
     class Meta:
@@ -25,10 +29,12 @@ class UsuarioRegistroSerializer(serializers.ModelSerializer):
             'sexo',
             'estado',
             'password',
-            'rol'
+            'rol',
+            'documentos'
         ]
 
     def create(self, validated_data):
+        documentos_data = validated_data.pop('documentos',[])
         password = validated_data.pop("password")
         rol = validated_data.pop("rol")  # Esto llega como una instancia o ID
 
@@ -37,5 +43,15 @@ class UsuarioRegistroSerializer(serializers.ModelSerializer):
             rol=rol,  # puede ser ID o instancia
             **validated_data
         )
+        
+        for doc in documentos_data:
+            DetalleDocumento.objects.create(
+                usuario=usuario,
+                documento_id = doc.get('id'),
+                numero=doc.get('numero')
+            )
+            
+        
+        
         return usuario
 
