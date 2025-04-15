03486ce72a5e5d85a6f24109d90afe5d4ca3db93
BoteritoROOT <d.sajor2003rp@gmail.com>
2025-04-15 00:20:28 -0400

    subiendo

--- userrs/controllers/controlles_usuario.py
+++ userrs/controllers/controlles_usuario.py
@@ -1,6 +1,7 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-from BaseDatos.models import Usuario
+from rest_framework import status
+from BaseDatos.models import Usuario, Rol
 from .serializers import UsuarioSerializer
 
 @api_view(['GET'])
@@ -9,4 +10,52 @@ def obtenerUsuarios(request):
     serializer = UsuarioSerializer(usuarios, many=True)
     return Response(serializer.data)
     
+
         
+@api_view(['PATCH'])
+def actualizar_usuario(request):
+    id_usuario = request.data.get('id')
+    if not id_usuario:
+        return Response({'error': 'El id es obligatorio para encontrar al usuario'}, status=status.HTTP_400_BAD_REQUEST)
+    
+    try:
+        usuarios = Usuario.objects.get(id=id_usuario)
+    except Usuario.DoesNotExist:
+         return Response({'error': 'Usuario no encontrado'}, status=status.HTTP_404_NOT_FOUND)
+     
+    campos_actualizados = []
+    
+    if 'nombre' in request.data:
+        usuarios.nombre = request.data['nombre']
+        campos_actualizados.append('nombre')
+    
+    if 'telefono' in request.data:
+        usuarios.telefono = request.data['telefono']
+        campos_actualizados.append('telefono')
+
+    if 'fecha_nacimiento' in request.data:
+        usuarios.fecha_nacimiento = request.data['fecha_nacimiento']
+        campos_actualizados.append('fecha_nacimiento')
+
+    if 'sexo' in request.data:
+        usuarios.sexo = request.data['sexo']
+        campos_actualizados.append('sexo')
+
+    if 'estado' in request.data:
+        usuarios.estado = request.data['estado']
+        campos_actualizados.append('estado')
+
+    if 'rol' in request.data:
+        try:
+            nuevo_rol = Rol.objects.get(id=request.data['rol'])
+            usuarios.rol = nuevo_rol
+            campos_actualizados.append('rol')
+        except Rol.DoesNotExist:
+            return Response({'error': 'Rol no encontrado'}, status=status.HTTP_404_NOT_FOUND)
+
+    usuarios.save()
+
+    return Response({
+        'mensaje': f"Usuario '{usuarios.nombre}' actualizado correctamente ✅",
+        'campos_actualizados': campos_actualizados
+    })
--- userrs/urls.py
+++ userrs/urls.py
@@ -4,8 +4,9 @@ from django.urls import path
 from .views import CrearRolView, RegistrarUsuarioViem,CrearDocumentoViem,CrearPrivilegioViem , LoginUsuarioView
 
 from .controllers.controller_bitacora import obtenerBitacora
-from .controllers.controlles_usuario import obtenerUsuarios
+from .controllers.controlles_usuario import obtenerUsuarios, actualizar_usuario
 from .controllers.controller_rol import obtenerRol
+
 
 urlpatterns = [
     path('rol/', CrearRolView.as_view(), name='crear_rol'),
@@ -17,6 +18,8 @@ urlpatterns = [
     
     path('/bitacora', obtenerBitacora, name="get_bitacoras"),
     path('/',obtenerUsuarios,name='obtener-usuarios'),
-    path('/roles',obtenerRol,name='obtener-rol')
+    path('/roles',obtenerRol,name='obtener-rol'),
+
+    path('/actualizr',actualizar_usuario)
     
 ]
