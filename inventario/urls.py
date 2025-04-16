9d68e50aaee1654ef2acfd0336b63b037a3a513b
BoteritoROOT <d.sajor2003rp@gmail.com>
2025-04-16 00:53:37 -0400

    actua

--- inventario/urls.py
+++ inventario/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     path('/categoria',CrearCategoriaView.as_view(), name='categoria' ),
     path('/almacen',CrearAlmacenView.as_view(), name='almacen' ),
     path('/producto',CrearProductoView.as_view(), name='producto' ),
-    path('mercaderia/crear/', CrearMercaderiaView.as_view(), name='crear-mercaderia'),
+    path('/mercaderia/crear', CrearMercaderiaView.as_view(), name='crear-mercaderia'),
     
     path('/obtener-marca', obtenerMarca, name='obtener-marca'),
     path('/obtener-almacen', obtenerAlmacen, name='obtener-almacen'),
