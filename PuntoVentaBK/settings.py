0f60491fd989fc1ec6c40832b28d72434db724e4
BoteritoROOT <d.sajor2003rp@gmail.com>
2025-04-16 13:00:08 -0400

    hola

--- PuntoVentaBK/settings.py
+++ PuntoVentaBK/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 import dj_database_url
 from dotenv import load_dotenv
 import os
+from datetime import timedelta
 
 load_dotenv()
 
@@ -62,6 +63,19 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
+    # Puedes agregar otros parámetros según la documentación
+}
+
 
 ROOT_URLCONF = 'PuntoVentaBK.urls'
 
