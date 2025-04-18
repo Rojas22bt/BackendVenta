8f00f664934b04e1fca9aa7d1e211669b61a0af8
BoteritoROOT <d.sajor2003rp@gmail.com>
2025-04-17 21:42:27 -0400

    holis 2

--- PuntoVentaBK/urls.py
+++ PuntoVentaBK/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
     # Otras rutas de tus apps
     path('api/usuario', include('userrs.urls')),
     path('api/inventario', include('inventario.urls')),
-    path('api/', include('venta.urls')),
+    path('api/venta', include('venta.urls')),
     # URLs para JWT
     # path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     # path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
--- venta/urls.py
+++ venta/urls.py
@@ -3,7 +3,7 @@ from .views import CrearMetodPagoView, CreateOfertaView, createFacturaView
 
 urlpatterns = [
     path('venta/metodoPago/', CrearMetodPagoView.as_view(), name='crear-pago'),
-    path('venta/oferta/', CreateOfertaView.as_view(), name='crear-pago'),
+    path('/oferta/register', CreateOfertaView.as_view(), name='crear-pago'),
     path('venta/factura/', createFacturaView.as_view(), name='factura-venta'),
     
 ]
