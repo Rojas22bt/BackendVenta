ea5a019d3d590840dc68b8673619efa47b1cdce2
BoteritoROOT <d.sajor2003rp@gmail.com>
2025-04-18 10:05:57 -0400

    holi

--- venta/urls.py
+++ venta/urls.py
@@ -5,7 +5,7 @@ from .controllers.controller_oferta import obtenerOfertas, obtenerOfertasActivas
 urlpatterns = [
     path('venta/metodoPago/', CrearMetodPagoView.as_view(), name='crear-pago'),
     path('/oferta/register', CreateOfertaView.as_view(), name='crear-pago'),
-    path('venta/factura/', createFacturaView.as_view(), name='factura-venta'),
+    path('/factura', createFacturaView.as_view(), name='factura-venta'),
     path('/crear-pago', crear_pago),
     path('/obtener-ofertas',obtenerOfertas, name='obtener-ofertas'),
     path('/obtener-ofertas-activas',obtenerOfertasActivas, name='obtener-ofertas-activas'),
